8be1222251318a601eab4553cbecb67c4f085553
Ramon Vasconcelos <ramonv.oliveira@gmail.com>
2019-11-11 14:52:16 -0300

    styling header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
+import './style.scss';
 import React from 'react';
 import Logo from '../../assets/images/logo.svg';
 
 const Header = () => {
   return (
     <div className="header">
-      <img src={Logo} />
+      <img className="logo" src={Logo} />
     </div>
   );
 }
