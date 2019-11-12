b3f9a72d430746dd12f03f747a85e2586b411867
Ramon Vasconcelos <ramonv.oliveira@gmail.com>
2019-11-11 22:06:03 -0300

    initial data fetching strucuture

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,29 @@
 import 'normalize.css';
 import './style.scss';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../Header/Header';
 import Card from '../Card/Card';
+import openWeather from '../../api/openWeather';
 
 const App = () => {
+  const [weather, setWeather] = useState(0);
+
+  async function fetchData() {
+    const response = await openWeather.get('/group', {
+      params: {
+        id: '3445709,3421319,184745',
+        appid: process.env.REACT_APP_API_KEY
+      }
+    })
+    .catch(err => console.log(`error ${err}`));
+
+    console.log(response.data.list)
+  };
+
+  useEffect(() => {
+    fetchData();
+  });
+
   return (
     <div className="app">
       <Header />
